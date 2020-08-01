9aaf323c9d4b0a47ed917b4f10728baf8a6f518e
omar <omar_omarsalama@yahoo.com>
2020-08-01 21:16:45 +0200

    initialized React/django project + set up an API call between the server and React

---
+++ frontend/gui/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import "antd/dist/antd.css";
+import { CustomLayout } from "./containers/Layout";
+import { ArticleList } from "./containers/ArticleList";
+
+function App() {
+  return (
+    <div className="App">
+      <CustomLayout>
+        <ArticleList />
+      </CustomLayout>
+    </div>
+  );
+}
+
+export default App;
